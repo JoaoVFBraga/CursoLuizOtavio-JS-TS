72abff3ffeec12429e1147b30c0e2f6b07a41314
JoaoVFBraga <143449194+JoaoVFBraga@users.noreply.github.com>
2023-11-27 17:22:46 -0300

    Add files via upload

---
+++ 01-JSBasico/a14-constmutavel/script.js
@@ -0,0 +1,5 @@
+/* Quando usamos const com tipos primitivos, esse valor nunca mais poderá ser alterado. Nesse caso, além de const não permitir reatribuição, o valor também é imutável (consequentemente, nunca podemos alterar essa constante).
+
+Já quando usamos const com valores mutáveis (como arrays e objetos), a variável continua sendo constante, porém os valores dentro do objeto poderão ser alterados. Isso acontece porque quando alteramos um valor interno de um objeto, não ocorre a reatribuição da variável com sinal de atribuição = (a variável continua apontando para o mesmo local na memória), apenas a alteração de um valor interno do mesmo objeto.
+
+Por este motivo, pode-se usar const com objetos mutáveis e ainda assim alterar seus valores internos. A única coisa que não vamos conseguir fazer é reatribuir o valor da variável. */
